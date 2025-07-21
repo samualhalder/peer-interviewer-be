227f56c28ed27cd4b840f3a8021049ac9528a081
samualhalder <samual.halder2020@gmail.com>
2025-07-21 12:04:02 +0530

    ratings

--- src/controllers/ratings.controllers.ts
+++ src/controllers/ratings.controllers.ts
@@ -1,21 +1,163 @@
-import { Request, Response } from "express"
-import prisma from "../startups/prisma.startup"
+import { Request, Response } from "express";
+import prisma from "../startups/prisma.startup";
+import HttpError from "../helpers/httpError.helper";
+import ResponseWrapper from "../helpers/response.helper";
 
-class RatingControllersClass{
-    give=async(req:Request,res:Response)=>{
-
+class RatingControllersClass {
+  getRatingByIntId = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const rating = await prisma.ratings.findFirst({
+      where: {
+        interviewId: id,
+      },
+    });
+    if (!rating) {
+      throw new HttpError(400, "No rating for this interview is given");
     }
-    delete=async(req:Request,res:Response)=>{
-
+    ResponseWrapper(res).status(200).body(rating).send();
+  };
+  give = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    if (!req.body.rating) {
+      throw new HttpError(400, "Rating field is required");
     }
-    update=async(req:Request,res:Response)=>{
-
+    const rating = await prisma.ratings.findFirst({
+      where: {
+        interviewId: id,
+      },
+    });
+    if (rating) {
+      throw new HttpError(400, "Already given rating for this interview");
     }
-    list=async(req:Request,res:Response)=>{
-
+    const intrerview = await prisma.interviewRequests.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (intrerview?.from != req.user?.id) {
+      throw new HttpError(
+        400,
+        "You are not allowed to give a rating on this interview"
+      );
     }
-    avg=async(req:Request,res:Response)=>{
-
+    const ratingInt = parseInt(req.body.rating);
+    if (isNaN(ratingInt) || ratingInt < 1 || ratingInt > 5) {
+      throw new HttpError(400, "Rating is invalid");
     }
+    await prisma.ratings.create({
+      data: {
+        userId: req.user?.id as string,
+        interviewerId: intrerview?.to as string,
+        interviewId: id,
+        rating: req.body.rating,
+        review: req.body.review,
+      },
+    });
+    ResponseWrapper(res).status(201).message("Rating is added").send();
+  };
+  delete = async (req: Request, res: Response) => {
+    const id = req.params.id;
+
+    const rating = await prisma.ratings.findFirst({
+      where: {
+        id: id,
+      },
+    });
+    if (!rating) {
+      throw new HttpError(400, "No such rating");
+    }
+    const intrerview = await prisma.interviewRequests.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (intrerview?.from != req.user?.id) {
+      throw new HttpError(
+        400,
+        "You are not allowed to delete this rating on this interview"
+      );
+    }
+
+    await prisma.ratings.delete({
+      where: {
+        id: id,
+      },
+    });
+    ResponseWrapper(res).status(201).message("Rating is deleted").send();
+  };
+  update = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const rating = await prisma.ratings.findFirst({
+      where: {
+        interviewId: id,
+      },
+    });
+    if (!rating) {
+      throw new HttpError(400, "No rating is given");
+    }
+
+    const intrerview = await prisma.interviewRequests.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (intrerview?.from != req.user?.id) {
+      throw new HttpError(
+        400,
+        "You are not allowed to give a rating on this interview"
+      );
+    }
+
+    const ratingInt = parseInt(req.body.rating);
+    if (isNaN(ratingInt) || ratingInt < 1 || ratingInt > 5) {
+      throw new HttpError(400, "Rating is invalid");
+    }
+    await prisma.ratings.update({
+      where: {
+        id: rating.id,
+      },
+      data: {
+        rating: ratingInt,
+        review: req.body.review,
+      },
+    });
+    ResponseWrapper(res).status(200).message("Rating is Updatd").send();
+  };
+  listGiven = async (req: Request, res: Response) => {
+    const ratings = await prisma.ratings.findMany({
+      where: {
+        userId: req.user?.id,
+      },
+      include: {
+        interview: true,
+        interviewer: true,
+      },
+    });
+    ResponseWrapper(res).status(200).body(ratings).send();
+  };
+  listRecived = async (req: Request, res: Response) => {
+    const ratings = await prisma.ratings.findMany({
+      where: {
+        interviewerId: req.user?.id,
+      },
+      include: {
+        interview: true,
+        User: true,
+      },
+    });
+    ResponseWrapper(res).status(200).body(ratings).send();
+  };
+  avg = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const result = await prisma.ratings.aggregate({
+      where: {
+        interviewerId: id,
+      },
+      _avg: {
+        rating: true,
+      },
+    });
+    ResponseWrapper(res).body(result).status(200).send();
+  };
 }
-export const RatingController=new RatingControllersClass()
+export const RatingController = new RatingControllersClass();
--- src/routes/ratings.routes.ts
+++ src/routes/ratings.routes.ts
@@ -2,13 +2,15 @@ import express from "express";
 import Auth from "../middlewares/auth.middleware";
 import { RatingController } from "../controllers/ratings.controllers";
 
-const router=express.Router()
+const router = express.Router();
 
 router
-.get('/list/:id',Auth,RatingController.list)
-.get('/avg/:id',Auth,RatingController.avg)
-.post('/add',Auth,RatingController.give)
-.put('/update/:id',Auth,RatingController.update)
-.delete('/delete/:id',Auth,RatingController.delete)
+  .get("/list-given", Auth, RatingController.listGiven)
+  .get("/list-recived", Auth, RatingController.listRecived)
+  .get("/get-by-interview/:id", Auth, RatingController.getRatingByIntId)
+  .get("/avg/:id", Auth, RatingController.avg) // interviewId(userId)
+  .post("/give/:id", Auth, RatingController.give) // interviewId
+  .put("/update/:id", Auth, RatingController.update) //interviewId
+  .delete("/delete/:id", Auth, RatingController.delete); //ratingId
 
-export default router
+export default router;
