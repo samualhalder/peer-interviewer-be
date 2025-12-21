5c1b53c30dcc44e6aa165b4679b0d5b2bc9834e5
Samual Halder <samual.halder2020@gmail.com>
2025-12-22 00:51:02 +0530

    implt:mailer

--- src/helpers/mail.helper.ts
+++ src/helpers/mail.helper.ts
@@ -2,11 +2,37 @@ import { Resend } from "resend";
 import loggerHelper from "./logger.helper";
 
 const apiKey = process.env.RESEND_API_KEY;
-const clientUrl = process.env.CLIENT_URL;
+const clientUrl = process.env.CLIENT_URL as string;
 const resend = new Resend(apiKey);
 
 export async function sendMail(mail: string, token: string) {
-  const body = `<div>Hello pls click below link to reset the password. <p>${clientUrl}/reset-password?token=${token}</p> </div>`;
+  const body = `
+<div>
+  <p>Hello, please click the link below to reset your password.</p>
+
+  <a
+    href="${clientUrl}/reset-password?token=${token}"
+    style="
+      display: inline-block;
+      padding: 10px 16px;
+      background-color: #2563eb;
+      color: #ffffff;
+      text-decoration: none;
+      border-radius: 6px;
+      font-weight: 600;
+    "
+  >
+    Reset Password
+  </a>
+
+  <p style="margin-top: 12px;">
+    Or copy this link:
+    <br />
+    ${clientUrl}/reset-password?token=${token}
+  </p>
+</div>
+`;
+
   const { data, error } = await resend.emails.send({
     from: "Peer Interviewer <onboarding@resend.dev>",
     to: [mail],
